9c6c93c56718193e34bffd66f38aef9a4bf3d0c6
unknown <APRENDIZ@BGDAFCGFFSP237.FormacionSena.RED>
2024-08-16 19:40:01 -0500

    carpeta

---
+++ ejercicio.js
@@ -0,0 +1,23 @@
+var fs = require('fs')
+
+
+/*fs.mkdirSync('Carpeta',(error)=>{
+    if(error){
+        throw error;
+}
+console.log('La carpeta se creo')
+})*/
+// fs.writeFileSync('./Carpeta/archivo.txt','Hola',(error)=>{
+//     if(error){
+//         throw error;
+// }
+// console.log('Archivo Creado')
+// })
+//fs.unlinkSync('./Carpeta/archivo.txt')
+// fs.rmdir('./Carpeta',(err) =>{
+//     if(err){
+//         console.error(err);
+//     } else{
+//         console.log('Carpeta eliminada')
+//     }
+// })
